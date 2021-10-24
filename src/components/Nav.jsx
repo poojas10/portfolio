32b97e860b4be79a79f10dd8555372ccb86c302f
pooja <36657906+poojas10@users.noreply.github.com>
2021-10-25 00:29:59 +0530

    logo changed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -27,7 +27,7 @@ export default function Nav() {
     <>
       <section className="section" tabIndex='0'>
         <header>
-          <h1 id='Logo'>Portfolio</h1>
+          <h1 id='Logo'>Pooja</h1>
           <button tabIndex="0" aria-label='btn' onClick={toggle} className='toggle-btn'>
           <svg  stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" height="2em" width="3em" xmlns="http://www.w3.org/2000/svg"><path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
           </button>
